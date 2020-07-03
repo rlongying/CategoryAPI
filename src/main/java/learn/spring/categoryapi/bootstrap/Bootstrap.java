bc6483063953e285303943c4940cd6f1e1f879b7
Mengjun(Ivan) Wang <wmj900823@gmail.com>
2020-07-02 23:57:31 -0700

    add customer bootstrap data

--- src/main/java/learn/spring/categoryapi/bootstrap/Bootstrap.java
+++ src/main/java/learn/spring/categoryapi/bootstrap/Bootstrap.java
@@ -1,7 +1,10 @@
 package learn.spring.categoryapi.bootstrap;
 
 import learn.spring.categoryapi.model.Category;
+import learn.spring.categoryapi.model.Customer;
 import learn.spring.categoryapi.repository.CategoryRepository;
+import learn.spring.categoryapi.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -10,19 +13,19 @@ import java.util.List;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class Bootstrap implements CommandLineRunner {
     private final CategoryRepository categoryRepository;
+    private final CustomerRepository customerRepository;
 
-    public Bootstrap(CategoryRepository categoryRepository) {
-        this.categoryRepository = categoryRepository;
-    }
 
     @Override
     public void run(String... args) throws Exception {
-        loadData();
+        loadCategoryData();
+        loadCustomerData();
     }
 
-    private void loadData() {
+    private void loadCategoryData() {
         Category fruits = new Category();
         fruits.setName("Fruits");
 
@@ -40,5 +43,23 @@ public class Bootstrap implements CommandLineRunner {
 
         categoryRepository.saveAll(List.of(fruits, dried, fresh, exotic, nuts));
         log.info("{} categories loaded.", categoryRepository.count());
+    }
+
+    private void loadCustomerData() {
+        Customer customer = new Customer();
+        customer.setFirstName("Ivan");
+        customer.setLastName("Wang");
+
+        Customer customer2 = new Customer();
+        customer2.setFirstName("Joe");
+        customer2.setLastName("Doe");
+
+        Customer customer3 = new Customer();
+        customer3.setFirstName("Jimmy");
+        customer3.setLastName("Chou");
+
+        customerRepository.saveAll(List.of(customer, customer2, customer3));
+        log.info("{} customers loaded", customerRepository.count());
+
     }
 }
